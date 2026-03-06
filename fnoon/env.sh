124538510564ed2987986be47b70cab2b2d7403a
Frederick Noon <fnoon@proton.me>
2026-03-05 20:55:41 -0800

    Update postgres 16 --> 17

--- fnoon/env.sh
+++ fnoon/env.sh
@@ -16,6 +16,6 @@
 #   ./scripts/init_redis.sh
 #   cargo build
 
-export PATH="/opt/homebrew/opt/postgresql@16/bin:$PATH"
-#export LDFLAGS="-L/opt/homebrew/opt/postgresql@16/lib"
-#export CPPFLAGS="-I/opt/homebrew/opt/postgresql@16/include"
+export PATH="/opt/homebrew/opt/postgresql@17/bin:$PATH"
+#export LDFLAGS="-L/opt/homebrew/opt/postgresql@17/lib"
+#export CPPFLAGS="-I/opt/homebrew/opt/postgresql@17/include"
